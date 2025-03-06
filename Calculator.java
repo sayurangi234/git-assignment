0778366930fe14bcbff32a61012b06d28d6d26c0
sayurangi234 <randikasayurangi@gmail.com>
2025-03-06 16:34:19 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(3, 3); 
+        System.out.println("Sum: " + sum); 
+    }
+}
